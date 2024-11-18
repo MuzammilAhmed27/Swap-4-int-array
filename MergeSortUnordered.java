bc6ab836b97e00c6bcbc52c759f8b16a1a4091ba
MuzammilAhmed27 <154408958+MuzammilAhmed27@users.noreply.github.com>
2024-11-19 01:26:43 +0500

    Add files via upload

---
+++ MergeSortUnordered.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bse045;
+
+/**
+ *
+ * @author For_all
+ */
+import java.util.Arrays;
+
+public class MergeSortUnordered {
+    public static void main(String[] args) {
+        int[] unorderedList = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Unordered list:");
+        printArray(unorderedList);
+        mergeSort(unorderedList, 0, unorderedList.length - 1);
+        System.out.println("Sorted list:");
+        printArray(unorderedList);
+    }
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+}
